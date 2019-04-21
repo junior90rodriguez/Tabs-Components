8a8ea766e8ad640b88bcd4f62f587cdb9bbb0223
Junior <junior.arodriguez@icloud.com>
2019-04-21 11:17:15 -0500

    added a click event listener

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,7 +14,9 @@ class TabLink {
      this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', function(){
+      this.select;
+    })
   };
 
   select() {
