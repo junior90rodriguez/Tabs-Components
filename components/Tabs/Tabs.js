a779d574786cc3bd4703cb2daab199228b38dec3
Junior <junior.arodriguez@icloud.com>
2019-04-21 11:37:20 -0500

    removed the class tabs-item-selected from each element

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -24,10 +24,9 @@ class TabLink {
     const links = document.querySelectorAll('.tabs-link'); 
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(function(e){
+    Array.from(links).forEach(e => {
       e.classList.remove('tabs-link-selected');
     })
-
     // Add a class named "tabs-link-selected" to this link
      this.element.classList.add('tabs-link-selected');
     
@@ -47,7 +46,10 @@ class TabItem {
      const items = document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
-    
+    Array.from(links).forEach(e => {
+      e.classList.remove('tabs-item-selected');
+    })
+
     // Add a class named "tabs-item-selected" to this element
     //this.element;
   }
